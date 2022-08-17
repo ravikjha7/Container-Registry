375c3f9ced8922772229036d3707e98ef7075e6a
Suyash Chavan <suyashc222@gmail.com>
2022-08-17 23:33:46 +0530

    Testing Workflow

---
+++ .github/scripts/linuxdiary-3.0/buildAndUpdate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while read p; do
+    IFS=' ' 
+    read -a line <<<"$p"
+    sha256sum ${line[0]} > ${line[1]}
+
+    IFS='/'
+    read -a strarr <<<"${line[0]}"
+
+    NAME_ARR=("${strarr[@]:5:2}")
+
+    NAME=$(IFS=_ ; echo "${NAME_ARR[*]}")
+
+    eval "docker build . -t ghcr.io/walchand-linux-users-group/${NAME}:latest -f ${line[0]}"
+    eval "docker push ghcr.io/walchand-linux-users-group/${NAME}:latest"
+done < ../../updates/linuxdiary-3.0
---
+++ .github/scripts/linuxdiary-3.0/linuxdiary-3.0.py
@@ -0,0 +1,48 @@
+import os
+import hashlib
+
+directory = os.environ['GITHUB_WORKSPACE'] + "/linuxdiary-3.0"
+updateFile = None
+
+def check(parent_dir, hash,dockerfile):
+    print("Checking " + dockerfile,end='')
+    sha256_hash = hashlib.sha256()
+
+    with open(dockerfile,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+    
+    if(not os.path.exists(hash)):
+        open(hash, 'w')
+    
+    with open(hash, 'r') as file:
+        fileHash = file.read().replace('\n', '')
+
+        fileHash = fileHash.split(' ')[0]
+
+        if(fileHash==sha256_hash.hexdigest()):
+            print(" - Not Modified")
+            return 
+        else:
+            print(" - Modified")
+            updateFile.write(dockerfile+" "+hash+"\n")
+
+def process(parent_dir, file):
+    if os.path.isfile(file):
+        check(parent_dir, parent_dir+"/hash", file)
+        return 
+
+    for filename in os.listdir(file):
+        if(filename!="hash"):
+            f = os.path.join(file, filename)
+            process(file, f)
+        
+
+def main():
+    global updateFile
+    updateFile = open(os.environ['GITHUB_WORKSPACE']+"/.github/updates/linuxdiary-3.0", "w")
+    process(directory, directory)
+    updateFile.close()
+
+if __name__=="__main__":
+    main()
